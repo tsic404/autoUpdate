6a261d9b6685ddd01847c70132d26e52f36c4392
Tsic <tsicliu@foxmail.com>
2022-06-07 13:56:32 +0800

    fix: fix fdm package error

--- infos/apps/org.freedownloadmanager.fdm/icons_fix.py
+++ infos/apps/org.freedownloadmanager.fdm/icons_fix.py
@@ -1,4 +1,4 @@
-from os import makedirs
+from os import makedirs, popen
 from shutil import copy
 
 
@@ -9,3 +9,7 @@ def do_extra(app):
     icons_path = 'opt/apps/' + appid + '/entries/icons/hicolor/256x256/apps/'
     makedirs(icons_path)
     copy(content + "/icon.png", icons_path + "freedownloadmanager.png" )
+    desktop = 'opt/apps/' + appid + '/entries/applications/freedownloadmanager.desktop'
+    shell = "sed -i 's|Icon=/opt/freedownloadmanager/icon.png|Icon=freedownloadmanager|g' " + desktop
+    popen(shell).read()
+
