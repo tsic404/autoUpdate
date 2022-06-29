310e2603d25e653ebabf7e1f0a9016f6fd334468
tsic404 <tsicliu@foxmail.com>
2022-06-29 15:30:19 +0800

    fix: ares commander desktop build

--- infos/apps/com.graebert.ares-commander/desktops_fix.py
+++ infos/apps/com.graebert.ares-commander/desktops_fix.py
@@ -8,5 +8,11 @@ def do_extra(app):
     desktops = ["graebert-gmbh_ares-commander.desktop","graebert-gmbh_ares-commander-scheme.desktop"]
     for i in desktops:
         copyfile(desktop_paths + i, desktop_entries + i)
-        shell = "sed -i 's|/opt/graebert-gmbh/ARES-Commander-2022/|/opt/apps/" + appid + "/files/|g' " + desktop_entries + i
+        shell = "sed -i 's|" + get_path(desktop_entries + i) +"|/opt/apps/" + appid + "/files/|g' " + desktop_entries + i
         popen(shell).read()
+
+def get_path(desktop):
+    shell = "cat " + desktop + """  | grep Path=| awk -F'=' '{print $2}'"""
+    res = popen(shell).read()
+    return res[:-6]
+    
