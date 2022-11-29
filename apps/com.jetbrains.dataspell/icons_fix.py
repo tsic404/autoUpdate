8198901458c65e60d3f890147355f974b389ce54
Liu Heng <liuheng@deepin.org>
2022-11-29 17:12:20 +0800

    test: test check
    test check
    
    Log:

---
+++ apps/com.jetbrains.dataspell/icons_fix.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+import os
+import shutil
+
+def do_extra(app):
+    app_id = app.appid
+    os.makedirs("opt/apps/" + app_id + "/entries/icons/hicolor/128x128/apps")
+    os.makedirs("opt/apps/" + app_id + "/entries/icons/hicolor/scable/apps")
+
+    shutil.copy("opt/apps/" + app_id + "/files/bin/dataspell.svg",
+                    "opt/apps/" + app_id + "/entries/icons/hicolor/scable/apps/com.jetbrains.dataspell.svg")
+    shutil.copy("opt/apps/" + app_id + "/files/bin/dataspell.png",
+                    "opt/apps/" + app_id + "/entries/icons/hicolor/128x128/apps/com.jetbrains.dataspell.png")
