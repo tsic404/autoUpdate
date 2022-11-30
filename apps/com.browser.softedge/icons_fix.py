d0cb06d807577d55efe1f3b3167e2acca12633b0
Tsic404 <tsicliu@icloud.com>
2022-11-30 17:21:11 +0800

    feat: add edge beta
    add edge beta
    
    Log:

---
+++ apps/com.browser.softedge/icons_fix.py
@@ -0,0 +1,11 @@
+from os import makedirs
+from shutil import copy
+
+def do_extra(app):
+    appid = app.appid
+    content = 'opt/apps/' + appid + '/files/'
+    icons_path = 'opt/apps/' + appid + '/entries/icons/hicolor/'
+    icons = ['16', '24', '32', '48', '128', '256']
+    for i in icons:
+        makedirs(icons_path + i + 'x' + i + '/apps/')
+        copy(content + 'product_logo_' + i + '_beta.png', icons_path + i + 'x' + i + '/apps/microsoft-edge-beta.png')
