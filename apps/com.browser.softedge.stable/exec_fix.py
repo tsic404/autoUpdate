ce8243cc15ee478a02793debf7195ca170b23aa2
Tsic404 <tsicliu@icloud.com>
2022-11-30 16:02:36 +0800

    feat: add edge stable
    add edge stable
    
    Log:

---
+++ apps/com.browser.softedge.stable/exec_fix.py
@@ -0,0 +1,8 @@
+from os import popen
+
+def do_extra(app):
+    appid = app.appid
+    print(appid)
+    desktop = 'opt/apps/' + appid + '/entries/applications/microsoft-edge.desktop'
+    shell = "sed -i 's/Exec=\/usr\/bin\/microsoft-edge-stable/Exec=\/opt\/apps\/com.browser.softedge.stable\/files\/microsoft-edge/g' " + desktop
+    popen(shell).read()
