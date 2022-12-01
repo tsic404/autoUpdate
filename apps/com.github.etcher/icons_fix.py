afbfdd4cd4edfa7008e5f7bd081d10b8466a0410
Tsic404 <tsicliu@icloud.com>
2022-12-01 15:05:33 +0800

    fix: fix etcher and popen

--- apps/com.github.etcher/icons_fix.py
+++ apps/com.github.etcher/icons_fix.py
@@ -1,7 +1,21 @@
-from contextvars import copy_context
+from os import listdir
+from os.path import join,isfile
+from shutil import copyfile, copytree
 
 def do_extra(app):
     appid = app.appid
     content = 'opt/apps/' + appid + '/files/'
     icons_path = 'opt/apps/' + appid + '/entries/icons'
-    copy_context(content + "/usr/share/icons", icons_path)
+    copy_content(content + "/usr/share/icons", icons_path)
+
+def copy_content(src, dest):
+    for content in listdir(src):
+        origin = join(src, content)
+        target = join(dest, content)
+        if isfile(origin):
+            try:
+                copyfile(origin, target)
+            except FileExistsError:
+                pass
+        else:
+            copytree(origin, target)
