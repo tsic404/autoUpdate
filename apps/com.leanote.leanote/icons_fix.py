2dbe789ad9694b3af508a01f10104ddf4d5dafcd
Tsic404 <tsicliu@icloud.com>
2022-12-01 15:38:41 +0800

    chore: sync old apps
    sync old apps
    
    log:

---
+++ apps/com.leanote.leanote/icons_fix.py
@@ -0,0 +1,8 @@
+from os import makedirs
+from shutil import move
+
+def do_extra(app):
+    appid = app.appid
+    makedirs("opt/apps/" + appid + "/entries/icons//hicolor/512x512/apps/")
+    move("opt/apps/" + appid + "/files/leanote.png",
+         "opt/apps/" + appid + "/entries/icons//hicolor/512x512/apps/leanote.png")
