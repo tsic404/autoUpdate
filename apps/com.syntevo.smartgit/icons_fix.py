82c9f4dd152a73201a3cd46a369d12a7ea757628
Tsic404 <tsicliu@icloud.com>
2022-12-01 19:43:57 +0800

    chore: sync old apps
    sync old apps
    
    log:

---
+++ apps/com.syntevo.smartgit/icons_fix.py
@@ -0,0 +1,15 @@
+from os import makedirs
+from shutil import copyfile
+
+
+def do_extra(app):
+    appid = app.appid
+    icons_size = ["32", "48", "64", "128", "256"]
+    icon_entry = "opt/apps/" + appid + "/entries/icons/hicolor/"
+    for i in icons_size:
+        makedirs(icon_entry + i + "x" + i + "/apps")
+        copyfile("opt/apps/" + appid + '/files/bin/smartgit-' + i + '.png',
+                    icon_entry + i + "x" + i + "/apps/smartgit.png")
+    makedirs(icon_entry + "scalable/apps")
+    copyfile("opt/apps/" + appid + "/files/bin/smartgit.svg",
+                icon_entry + "scalable/apps/smartgit.svg")
---
+++ apps/com.vivaldi.vivaldi/exec_fix.py
@@ -0,0 +1,6 @@
+from os import popen
+def do_extra(app):
+    appid = app.appid
+    desktop = 'opt/apps/' + appid + '/entries/applications/vivaldi-stable.desktop'
+    shell = "sed -i 's|Exec=/usr/bin/vivaldi-stable|Exec=/opt/apps/com.vivaldi.vivaldi/files/vivaldi|g' " + desktop
+    popen(shell).read()
