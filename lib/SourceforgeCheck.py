f6de972953b6cd151679fc67e0dc3638aefa22a9
Tsic404 <tsicliu@icloud.com>
2022-12-01 14:11:29 +0800

    fix: add env to popen

---
+++ lib/SourceforgeCheck.py
@@ -0,0 +1,17 @@
+
+import os
+from requests import get
+from re import search
+def check(sourceforge):
+    project_name = sourceforge["project"]
+    d_filename = sourceforge["d_filename"]
+    url = "http://sourceforge.net/projects/{PROJECT_NAME}"
+    url = url.replace("{PROJECT_NAME}" ,project_name)
+    res = get(url + "/best_release.json").json()
+    filename = res['platform_releases']['linux']['filename']
+
+    dl_url = "https://sourceforge.net/projects/" + project_name + "/files" + filename + "/download"
+    dl_url = get(dl_url, stream=True, allow_redirects=False).headers['Location']
+    ver = search(d_filename, os.path.basename(filename)).groups()[0]
+
+    print(sourceforge['arch'], ver, dl_url)
