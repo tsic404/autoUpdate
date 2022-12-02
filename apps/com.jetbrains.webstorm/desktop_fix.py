c2693e89f5370a725f292fd4e795a7c9bcded792
Tsic404 <tsicliu@icloud.com>
2022-12-02 17:15:21 +0800

    tmp remove

--- apps/com.jetbrains.webstorm/desktop_fix.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-def do_extra(app):
-    appid = app.appid
-    dirname, filename = os.path.split(os.path.abspath(__file__))
-    applications_path ='opt/apps/' + appid + '/entries/applications/'
-    shutil.copy(dirname + '/' + appid + '.desktop', applications_path)
