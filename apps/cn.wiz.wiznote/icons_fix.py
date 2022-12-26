65362dd8badbb442c3dd254a586f42fc9b2a39ec
Liu Heng <liuheng@deepin.org>
2022-12-26 18:06:46 +0800

    fix: fix icon error

--- apps/cn.wiz.wiznote/icons_fix.py
+++ apps/cn.wiz.wiznote/icons_fix.py
@@ -3,7 +3,5 @@ import shutil
 
 def do_extra(app):
     appid = app.appid
-    shutil.rmtree('opt/apps/' + appid + '/entries/icons/hicolor/720x720/')
     shutil.rmtree('opt/apps/' + appid + '/entries/icons/hicolor/1024x1024/')
-    shutil.rmtree('opt/apps/' + appid + '/files/usr/share/icons/hicolor/720x720/')
     shutil.rmtree('opt/apps/' + appid + '/files/usr/share/icons/hicolor/1024x1024/')
