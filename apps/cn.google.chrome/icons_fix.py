4ebf809073ab17f1f953ee10370ad7d82dd8ccd5
Liu Heng <liuheng@deepin.org>
2023-01-29 13:42:05 +0800

    test: test run as non root

--- apps/cn.google.chrome/icons_fix.py
+++ apps/cn.google.chrome/icons_fix.py
@@ -6,6 +6,7 @@ def do_extra(app):
     content = 'opt/apps/' + appid + '/files/'
     icons_path = 'opt/apps/' + appid + '/entries/icons/hicolor/'
     icons = ['16', '24', '32', '48', '128', '256']
+
     for i in icons:
         makedirs(icons_path + i + 'x' + i + '/apps/')
         copy(content + 'product_logo_' + i + '.png', icons_path + i + 'x' + i + '/apps/google-chrome.png')
